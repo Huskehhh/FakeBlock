a961d8b48b503b2f9abe5b32a5d5dc1ca08c5f11
Jordyn Newnham <me@huskehhh.com>
2019-06-25 16:32:37 +0800

    remove unused import

--- src/com/huskehhh/fakeblock/listeners/FakeBlockListener.java
+++ src/com/huskehhh/fakeblock/listeners/FakeBlockListener.java
@@ -5,7 +5,6 @@ import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.*;
 
 public class FakeBlockListener implements Listener {
